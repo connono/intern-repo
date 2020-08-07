777f36855d4604d405f6a9e98941b5e12b76ac59
yinshi.wyl <yinshi.wyl@antfin.com>
2020-08-07 15:46:21 +0800

    feat: add groupby & orderby

--- src/query.ts
+++ src/query.ts
@@ -1,8 +1,11 @@
 import { Data, Field } from './types';
+import _ from 'lodash';
 
 export class Query {
 
   private data;
+
+  public options;
 
   constructor(data: Data) {
     this.data = data;
@@ -15,6 +18,16 @@ export class Query {
   public select(...fields: Field[]): Query {
 
     // TODO
+    fields.map((field)=>{
+      this.options.select.push(field);
+      if (field.aggregate!=='raw') {
+        this.options.select.union = true;
+        this.options.select.unionField = field.aggregate;
+      } else {
+        // ...
+      }
+    })
+    this.options.select = fields; 
 
     return this;
   }
@@ -24,9 +37,11 @@ export class Query {
    * @param field 
    * @param asc 
    */
-  public orderBy(fields: string, asc?: boolean): Query {
+  public orderBy(field: string, asc?: boolean): Query {
 
     // TODO
+    this.options.orderBy.field = field;
+    this.options.orderBy.asc = asc ? 'desc' : 'asc';
     
     return this;
   }
@@ -39,6 +54,7 @@ export class Query {
   public groupBy(fields: string): Query {
 
     // TODO
+    this.options.groupBy = fields;
     
     return this;
   }
@@ -50,6 +66,7 @@ export class Query {
   public limit(n: number): Query {
 
     // TODO
+    this.options.limit = n;
     
     return this;
   }
@@ -59,7 +76,45 @@ export class Query {
    */
   public record(): Data {
 
+    const filter = (array, func) => {
+      const filterArray = [];
+      array.map((item)=>{
+        if (func(item)) {
+          filterArray.push(item);
+        }
+      });
+      return filterArray;
+    }
+
+    const selectVal = (data, field) => {
+      const uniqueFilterNames = Array.from(new Set(data.map((item)=>item[field])));
+      return uniqueFilterNames.map((name)=>filter(data,(item)=>{item[field]===name}));
+    }
+
     // TODO
+    
+    let data = this.data;
+
+    //先处理orderBy
+    if(!_.isEmpty(this.options.orderBy)){
+      const { field, asc } = this.options.orderBy;
+      if (asc === 'asc'){
+        data.sort((a, b) => {return a[field]-b[field]});
+      } else {
+        data.sort((a, b) => {return b[field]-a[field]});
+      }
+    }
+
+    // 再处理groupBy
+    if(!_.isEmpty(this.options.groupBy)){
+      const fields = this.options.groupBy.fields;
+      fields.map((field)=>{
+        data = [].concat(selectVal(data, field));
+      })
+    }
+    
+    
+
     
     return [];
   }
